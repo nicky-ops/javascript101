f49a3cbcc61f3a10e64f01b18ff6173d02efe66e
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-03-29 23:05:06 +0300

    javascrtipt file

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// const elems= document.querySelectorAll(".class1");
+// console.log(elems)
+
+function addNumbers(num1, num2){
+    const total= Number(num1)+ Number(num2);
+    return total;
+}
+const addedTotal= addNumbers("20", 8)
+
+function multiply ( a, b){
+    const product = Number(a)* Number(b);
+    return product
+}
+const allProduct= multiply(1, 14);
