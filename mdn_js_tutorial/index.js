111204da4ea04d3c525cb9dbd2d607552a4e75ea
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-04-03 17:25:45 +0300

    numbers game

--- mdn_js_tutorial/index.js
+++ mdn_js_tutorial/index.js
@@ -1,7 +1,19 @@
-const button = document.querySelector('button');
-button.addEventListener("click", updateName);
+// Function: creates a new paragraph and appends it to the bottom of the html body
 
-function updateName() {
-    const name = prompt("Enter a new name");
-    button.textContent = `Player 1: ${name}`;
+function createParagraph() {
+    const para = document.createElement('p');
+    para.textContent = "You clicked the button!";
+    document.body.appendChild(para);
+}
+
+/*
+1. Get references to all the buttons on the page in an array format.
+2. Loop through all the buttons and add a click event listener to ech one.
+When any button is clicked, the createParagraph() function is run.
+*/
+
+const buttons = document.querySelectorAll('button');
+
+for (const button of buttons) {
+    button.addEventListener("click", createParagraph)
 }
