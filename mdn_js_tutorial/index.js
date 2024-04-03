3d7298c73b7a555e1708b9fd63b721aca84cf269
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-04-03 09:18:50 +0300

    javascript practice

---
+++ mdn_js_tutorial/index.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('button');
+button.addEventListener("click", updateName);
+
+function updateName() {
+    const name = prompt("Enter a new name");
+    button.textContent = `Player 1: ${name}`;
+}
