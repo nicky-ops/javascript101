ea9ffbda33dd180053a853f40b98e7fd5b548730
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-06-06 13:30:03 +0300

    generic types

--- alx-javascript/typescript.ts
+++ alx-javascript/typescript.ts
@@ -20,3 +20,57 @@ class UserAccount {
 }
 
 const user1: User = new UserAccount("Murphy", 1);
+
+// composing types
+// Unions
+type MyBool = true | false;
+type WindowStates = "open" | "closed" | "minimized";
+type LockStates = "locked" | "unlocked";
+type PositiveOddNumbersUnderTen = 1 | 3 | 5 | 7 | 9;
+
+function getLength(obj: string | string[]) {
+    return obj.length;
+}
+
+function wrapInArray(obj: string | string[]) {
+    if (typeof obj === "string") {
+        return [obj];
+                (parameter) obj: string
+    }
+    return obj;
+}
+
+// Generics
+type StringArray = Array<string>;
+type NumberArray = Array<number>;
+type ObjectWithNameArray = Array<{ name: string }>;
+
+
+// Declaring your own types that use generics
+interface Backpack<Type> {
+    add: (obj: Type) => void;
+    get: () => Type;
+}
+
+// This line is a shortcut to tell TypeScript there is a constant called 'backpack', and to not worry about where it came from
+declare const backpack: Backpack<string>;
+
+// object is a string, because we declared it above as the variable part of Backpack
+const object = backpack.get();
+
+// Since the backpack variable is a sting, you can't pass a number to the add function.
+backpack.add(23);
+
+// structural type system
+interface Point {
+    x: number;
+    y: number;
+}
+
+function logPoint(p: Point) {
+    console.log(`${p.x}, ${p.y}`);
+}
+
+// logs "12, 26"
+const point = { x: 12, y: 26 };
+logPoint(point);
